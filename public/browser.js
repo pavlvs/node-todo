059149e8b09a5f24597026bd753aec4bb81e83ce
Paul J Shaw <pavlvsxavier@gmail.com>
2020-06-10 09:07:12 +0100

    added functionality to delete items

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,13 @@
 document.addEventListener('click', e => {
+    // Delete feature
+    if (e.target.classList.contains('delete-me')) {
+        if (confirm('Are you sure?')) {
+            axios.post('/delete-item', { id: e.target.getAttribute('data-id') }).then(() => {
+                e.target.parentElement.parentElement.remove()
+            }).catch(() => console.log('Please try again later'))
+        }
+    }
+    // Update feature
     if (e.target.classList.contains('edit-me')) {
         let userInput = prompt('Enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML)
         if (userInput) {
--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
             return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                             <span class="item-text">${item.text}</span>
                             <div>
-                            <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button> <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                            <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button> <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
                             </div>
                         </li> `
         }).join('')}
@@ -67,6 +67,12 @@ app.post('/create-item', (req, res) => {
 
 app.post('/update-item', (req, res) => {
     db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, () => {
+        res.send('Success')
+    })
+});
+
+app.post('/delete-item', (req, res) => {
+    db.collection('items').deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, () => {
         res.send('Success')
     })
 });
