a883d83b11170c483a993de733110c72e2febe21
Paul J Shaw <pavlvsxavier@gmail.com>
2020-06-10 02:45:50 +0100

    Added functionality to update items

--- public/browser.js
+++ public/browser.js
@@ -1,8 +1,10 @@
 document.addEventListener('click', e => {
     if (e.target.classList.contains('edit-me')) {
-        let userInput = prompt('Enter your desired new text')
-        axios.post('/update-item', { text: userInput }).then(() => {
-            // TODO: something for later
-        }).catch(() => console.log('Please try again later'))
+        let userInput = prompt('Enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML)
+        if (userInput) {
+            axios.post('/update-item', { text: userInput, id: e.target.getAttribute('data-id') }).then(() => {
+                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput
+            }).catch(() => console.log('Please try again later'))
+        }
     }
 })
